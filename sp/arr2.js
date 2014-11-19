1e26773a32a954e92e497643e5776caa875cb636
MohamedOs <mos4ma@gmail.com>
2014-11-19 04:44:20 +0200

    Update arr2.js

--- sp/arr2.js
+++ sp/arr2.js
@@ -1,4 +1,4 @@
 var arrBold2 = [
-{"title":"الدوري الأسباني","href":"http://www.kooora.com/?c=10814&cm=m","image":"http://o.kooora.com/i/khome2.png"},
-{"title":"كأس ملك أسبانيا","href":"http://www.kooora.com/?c=10817&cm=m","image":"http://o.kooora.com/i/khome2.png"},
+{"title":"الدوري المصري الممتاز","href":"http://www.goal.com/ar-eg/tables/premier-league/206","image":"http://o.kooora.com/i/khome2.png"},
+{"title":"الدوري المصري الممتاز","href":"http://www.goal.com/ar-eg/tables/premier-league/206","image":"http://o.kooora.com/i/khome2.png"},
 ];
