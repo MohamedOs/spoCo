6997d96138e57062e0c2733eefe5795099a213e0
MohamedOs <mos4ma@gmail.com>
2014-11-20 22:00:44 +0200

    Update arr1.js

--- sp/arr1.js
+++ sp/arr1.js
@@ -2,11 +2,11 @@ var arrBold = [
 {
 "title":"الدوري الأسباني",
 "href":"http://www.kooora.com/?c=10814&cm=m",
-"image":"http://www.radioma3ak.com/news/wp-content/uploads/2014/08/barca1.jpg"
+"image":"http://o.kooora.com/i/khome2.png"
 },
 {
 "title":"كأس ملك أسبانيا",
 "href":"http://www.kooora.com/?c=10817&cm=m",
-"image":"http://www.hibasport.com/upload/17102014-f0cdc.jpg"
+"image":"http://o.kooora.com/i/khome2.png"
 },
 ];
