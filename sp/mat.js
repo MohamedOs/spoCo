bbb9a898281230ccec5b36566f3a8273d1f22533
MohamedOs <mos4ma@gmail.com>
2014-11-21 05:12:03 +0200

    Update mat.js

--- sp/mat.js
+++ sp/mat.js
@@ -23,7 +23,7 @@ var handler = new htmlparser.DefaultHandler(function(err, dom) {
         alert('Error: ' + err);
     } else {
         varsM.data=select(dom,'script'); 
-        var dataNeu2=varsM.data[7].children[0].data;
+        var dataNeu2=varsM.data[7].children[0].data+varsM.data[8].children[0].data;
         dataNeu2=dataNeu2.split('var lang = 0;')[1];
         dataNeu2=dataNeu2.split('mfooter("");')[0];
         dataNeu2=dataNeu2.slice(2, -2);
