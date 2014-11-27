7409edaba12fcfe57fdf2cd5dc2c048499a3c7fc
MohamedOs <mos4ma@gmail.com>
2014-11-28 01:04:25 +0200

    Update arr1.js

--- eng/arr1.js
+++ eng/arr1.js
@@ -5,11 +5,11 @@ var arrBold = [
 "image":"http://img2.kooora.com/?i=albums%2fmatches%2f747311%2f2014-11-01t210855z_440413342_gm1eab20dyq01_rtrmadp_3_soccer-spain_reuters.jpg"
 },
 {
-"title":"الدوري الإنجليزي الأول"
+"title":"الدوري الإنجليزي الأول",
 "href":"http://www.kooora.com/?c=10527&cm=m",
 "image":"http://img3.kooora.com/?i=albums%2fmatches%2f997973%2f2014-11-04-04476522_epa.jpg"
 },{
-"title":"كأس الإتحاد الإنجليزي"
+"title":"كأس الإتحاد الإنجليزي",
 "href":"http://www.kooora.com/?c=10903&cm=m",
 "image":"http://img3.kooora.com/?i=albums%2fmatches%2f997973%2f2014-11-04-04476522_epa.jpg"
 },
