6a7f95bedc1055c706ea20059612a8a9eb9a4271
MohamedOs <mos4ma@gmail.com>
2014-11-28 00:54:27 +0200

    Update arr2.js

--- eng/arr2.js
+++ eng/arr2.js
@@ -1,7 +1,15 @@
 var arrBold2 = [
 {
-"title":"الدوري الأسباني",
-"href":"http://www.goal.com/ar-eg/tables/%D8%A7%D9%84%D8%AF%D9%88%D8%B1%D9%8A-%D8%A7%D9%84%D8%A7%D8%B3%D8%A8%D8%A7%D9%86%D9%8A-%D8%A7%D9%84%D8%AF%D8%B1%D8%AC%D8%A9-%D8%A7%D9%84%D8%A3%D9%88%D9%84%D9%89/7?ICID=CL",
+"title":"الدوري الإنجليزي الممتاز",
+"href":"http://www.goal.com/ar-eg/tables/%D8%A7%D9%84%D8%AF%D9%88%D8%B1%D9%8A-%D8%A7%D9%84%D8%A5%D9%86%D8%AC%D9%84%D9%8A%D8%B2%D9%8A-%D8%A7%D9%84%D9%85%D9%85%D8%AA%D8%A7%D8%B2/8?ICID=CL",
+"image":"http://img2.kooora.com/?i=albums%2fmatches%2f747311%2f2014-11-01t210855z_440413342_gm1eab20dyq01_rtrmadp_3_soccer-spain_reuters.jpg"
+},{
+"title":"U21 Premier League Division 1",
+"href":"http://www.goal.com/ar-eg/tables/u21-premier-league-division-1/1058?ICID=CL",
+"image":"http://img2.kooora.com/?i=albums%2fmatches%2f747311%2f2014-11-01t210855z_440413342_gm1eab20dyq01_rtrmadp_3_soccer-spain_reuters.jpg"
+},{
+"title":"دوري البطولة الإنجليزية",
+"href":"http://www.goal.com/ar-eg/tables/%D8%AF%D9%88%D8%B1%D9%8A-%D8%A7%D9%84%D8%A8%D8%B7%D9%88%D9%84%D8%A9-%D8%A7%D9%84%D8%A5%D9%86%D8%AC%D9%84%D9%8A%D8%B2%D9%8A%D8%A9-%D8%A7%D9%84%D8%AF%D8%B1%D8%AC%D8%A9-%D8%A7%D9%84%D8%A3%D9%88%D9%84%D9%89/70?ICID=CL",
 "image":"http://img2.kooora.com/?i=albums%2fmatches%2f747311%2f2014-11-01t210855z_440413342_gm1eab20dyq01_rtrmadp_3_soccer-spain_reuters.jpg"
 },
 ];
