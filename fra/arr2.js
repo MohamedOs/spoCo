8818a38f6d269f0322d094d2a32b90c6b7ea70c8
MohamedOs <mos4ma@gmail.com>
2014-11-28 01:30:27 +0200

    Update arr2.js

--- fra/arr2.js
+++ fra/arr2.js
@@ -1,7 +1,11 @@
 var arrBold2 = [
 {
-"title":"الدوري الأسباني",
-"href":"http://www.goal.com/ar-eg/tables/%D8%A7%D9%84%D8%AF%D9%88%D8%B1%D9%8A-%D8%A7%D9%84%D8%A7%D8%B3%D8%A8%D8%A7%D9%86%D9%8A-%D8%A7%D9%84%D8%AF%D8%B1%D8%AC%D8%A9-%D8%A7%D9%84%D8%A3%D9%88%D9%84%D9%89/7?ICID=CL",
+"title":"الدوري الفرنسي - الدرجة الأولى",
+"href":"http://www.goal.com/ar-eg/tables/%D8%A7%D9%84%D8%AF%D9%88%D8%B1%D9%8A-%D8%A7%D9%84%D9%81%D8%B1%D9%86%D8%B3%D9%8A-%D8%A7%D9%84%D8%AF%D8%B1%D8%AC%D8%A9-%D8%A7%D9%84%D8%A3%D9%88%D9%84%D9%89/16?ICID=CL",
+"image":"http://img2.kooora.com/?i=albums%2fmatches%2f747311%2f2014-11-01t210855z_440413342_gm1eab20dyq01_rtrmadp_3_soccer-spain_reuters.jpg"
+},{
+"title":"الدوري الفرنسي - الدرجة الثانية",
+"href":"http://www.goal.com/ar-eg/tables/%D8%A7%D9%84%D8%AF%D9%88%D8%B1%D9%8A-%D8%A7%D9%84%D9%81%D8%B1%D9%86%D8%B3%D9%8A-%D8%A7%D9%84%D8%AF%D8%B1%D8%AC%D8%A9-%D8%A7%D9%84%D8%AB%D8%A7%D9%86%D9%8A%D8%A9/17?ICID=CL",
 "image":"http://img2.kooora.com/?i=albums%2fmatches%2f747311%2f2014-11-01t210855z_440413342_gm1eab20dyq01_rtrmadp_3_soccer-spain_reuters.jpg"
 },
 ];
