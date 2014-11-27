873eb82d4ebbee43f8b3ab11f9214ac9e073925c
MohamedOs <mos4ma@gmail.com>
2014-11-27 21:29:08 +0200

    Update arr1.js

--- alg/arr1.js
+++ alg/arr1.js
@@ -2,11 +2,11 @@ var arrBold = [
 {
 "title":"دوري موبيليس للمحترفين الاول",
 "href":"http://www.kooora.com/?c=10764&cm=m",
-"image":"http://img2.kooora.com/?i=albums%2fmatches%2f747311%2f2014-11-01t210855z_440413342_gm1eab20dyq01_rtrmadp_3_soccer-spain_reuters.jpg"
+"image":"https://cdn4.iconfinder.com/data/icons/Iconshock_soccer/png/256/soccer_5.png"
 },
 {
 "title":"الرابطة المحترفة الجزائرية الثانية",
 "href":"http://www.kooora.com/?c=10791&cm=m",
-"image":"http://cdn3.howtogeek.com/wp-content/uploads/2011/02/footballsoccercustomizationset00.png"
+"image":"https://cdn4.iconfinder.com/data/icons/pretty-office-part-7-reflection-style/256/Football-pitch.png"
 },
 ];
