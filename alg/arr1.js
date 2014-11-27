f69ee9444a3cdecef380496fcda134a49ca5ed45
MohamedOs <mos4ma@gmail.com>
2014-11-27 20:46:24 +0200

    Update arr1.js

--- alg/arr1.js
+++ alg/arr1.js
@@ -1,12 +1,12 @@
 var arrBold = [
 {
-"title":"دوري موبيليس للمحترفين الاول",
-"href":"http://www.kooora.com/?c=10764&cm=m",
-"image":"http://img.kooora.com/?i=reuters%2f2014-06-29%2f2014-06-29t210304z_64640197_tb3ea6t1p4mma_rtrmadp_3_brazil-worldcup_reuters.jpg"
+"title":"الدوري الأسباني",
+"href":"http://www.kooora.com/?c=10814&cm=m",
+"image":"http://img2.kooora.com/?i=albums%2fmatches%2f747311%2f2014-11-01t210855z_440413342_gm1eab20dyq01_rtrmadp_3_soccer-spain_reuters.jpg"
 },
 {
-"title":"الرابطة المحترفة الجزائرية الثانية,
-"href":"http://www.kooora.com/?c=10791",
-"image":"http://img.kooora.com/?i=reuters%2f2014-06-26%2f2014-06-26t221925z_1247471049_tb3ea6q1q2poa_rtrmadp_3_soccer-world-m48-alg-rus_reuters.jpg"
+"title":"كأس ملك أسبانيا",
+"href":"http://www.kooora.com/?c=10817&cm=m",
+"image":"http://img3.kooora.com/?i=albums%2fmatches%2f997973%2f2014-11-04-04476522_epa.jpg"
 },
 ];
