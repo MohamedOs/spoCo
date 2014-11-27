e5ce8da63e3cce408879e20048157205677233d7
MohamedOs <mos4ma@gmail.com>
2014-11-27 20:46:38 +0200

    Update arr2.js

--- alg/arr2.js
+++ alg/arr2.js
@@ -1,14 +1,7 @@
 var arrBold2 = [
 {
-"title":"الدوري الجزائري - الدرجة الأولى",
-"href":"http://www.goal.com/ar-eg/tables/%D8%A7%D9%84%D8%AF%D9%88%D8%B1%D9%8A-%D8%A7%D9%84%D8%AC%D8%B2%D8%A7%D8%A6%D8%B1%D9%8A-%D8%A7%D9%84%D8%AF%D8%B1%D8%AC%D8%A9-%D8%A7%D9%84%D8%A3%D9%88%D9%84%D9%89/205?ICID=CL",
-"image":"http://img.kooora.com/?i=reuters%2f2014-06-26%2f2014-06-26t221925z_1247471049_tb3ea6q1q2poa_rtrmadp_3_soccer-world-m48-alg-rus_reuters.jpg"
-  
-},
-{
-"title":"الدوري الجزائري الدرجة الثانية",
-"href":"http://www.goal.com/ar-eg/tables/%D8%A7%D9%84%D8%AF%D9%88%D8%B1%D9%8A-%D8%A7%D9%84%D8%AC%D8%B2%D8%A7%D8%A6%D8%B1%D9%8A-%D8%A7%D9%84%D8%AF%D8%B1%D8%AC%D8%A9-%D8%A7%D9%84%D8%AB%D8%A7%D9%86%D9%8A%D8%A9/207?ICID=CL",
-"image":"http://img.kooora.com/?i=reuters%2f2014-06-29%2f2014-06-29t210304z_64640197_tb3ea6t1p4mma_rtrmadp_3_brazil-worldcup_reuters.jpg"
-  
+"title":"الدوري الأسباني",
+"href":"http://www.goal.com/ar-eg/tables/%D8%A7%D9%84%D8%AF%D9%88%D8%B1%D9%8A-%D8%A7%D9%84%D8%A7%D8%B3%D8%A8%D8%A7%D9%86%D9%8A-%D8%A7%D9%84%D8%AF%D8%B1%D8%AC%D8%A9-%D8%A7%D9%84%D8%A3%D9%88%D9%84%D9%89/7?ICID=CL",
+"image":"http://img2.kooora.com/?i=albums%2fmatches%2f747311%2f2014-11-01t210855z_440413342_gm1eab20dyq01_rtrmadp_3_soccer-spain_reuters.jpg"
 },
 ];
