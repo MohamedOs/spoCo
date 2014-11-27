82d6730469fa84b718a7a6ba55a23c774f69c57c
MohamedOs <mos4ma@gmail.com>
2014-11-27 21:29:03 +0200

    Update arr2.js

--- alg/arr2.js
+++ alg/arr2.js
@@ -2,10 +2,10 @@ var arrBold2 = [
 {
 "title":"الدوري الجزائري - الدرجة الأولى",
 "href":"http://www.goal.com/ar/tables/%D8%A7%D9%84%D8%AF%D9%88%D8%B1%D9%8A-%D8%A7%D9%84%D8%AC%D8%B2%D8%A7%D8%A6%D8%B1%D9%8A-%D8%A7%D9%84%D8%AF%D8%B1%D8%AC%D8%A9-%D8%A7%D9%84%D8%A3%D9%88%D9%84%D9%89/205?ICID=CL",
-"image":"http://cdn3.howtogeek.com/wp-content/uploads/2011/02/footballsoccercustomizationset00.png"
+"image":"https://cdn4.iconfinder.com/data/icons/Iconshock_soccer/png/256/soccer_5.png"
 },{
 "title":"الدوري الجزائري الدرجة الثانية",
 "href":"http://www.goal.com/ar/tables/%D8%A7%D9%84%D8%AF%D9%88%D8%B1%D9%8A-%D8%A7%D9%84%D8%AC%D8%B2%D8%A7%D8%A6%D8%B1%D9%8A-%D8%A7%D9%84%D8%AF%D8%B1%D8%AC%D8%A9-%D8%A7%D9%84%D8%AB%D8%A7%D9%86%D9%8A%D8%A9/207?ICID=CL",
-"image":"http://www.3dtotal.com/tutorial/3d_studio_max/creating_and_texturing_a_football/images/image_17_final_render.jpg"
+"image":"https://cdn4.iconfinder.com/data/icons/pretty-office-part-7-reflection-style/256/Football-pitch.png"
 },
 ];
