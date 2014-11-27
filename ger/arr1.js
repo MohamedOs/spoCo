0f0d0aba1953420ee6c38fdb4a600d8fdff23b08
MohamedOs <mos4ma@gmail.com>
2014-11-28 01:53:47 +0200

    Update arr1.js

--- ger/arr1.js
+++ ger/arr1.js
@@ -1,12 +1,12 @@
 var arrBold = [
 {
-"title":"الدوري الأسباني",
-"href":"http://www.kooora.com/?c=10814&cm=m",
+"title":"الدوري الألماني",
+"href":"http://www.kooora.com/?c=10564&cm=m",
 "image":"http://img2.kooora.com/?i=albums%2fmatches%2f747311%2f2014-11-01t210855z_440413342_gm1eab20dyq01_rtrmadp_3_soccer-spain_reuters.jpg"
 },
 {
-"title":"كأس ملك أسبانيا",
-"href":"http://www.kooora.com/?c=10817&cm=m",
+"title":"كأس ألمانيا",
+"href":"http://www.kooora.com/?c=10137&cm=m",
 "image":"http://img3.kooora.com/?i=albums%2fmatches%2f997973%2f2014-11-04-04476522_epa.jpg"
 },
 ];
